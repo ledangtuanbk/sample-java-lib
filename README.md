01f926a832f8d9cd973c884dc3aa141c56758097
tuanld <ledangtuanbk@gmail.com>
2022-05-29 21:34:42 +0700

    init project

---
+++ README.md
@@ -0,0 +1,1 @@
+# sample-java-lib
---
+++ src/main/java/com/ldt/mylib/MyLib.java
@@ -0,0 +1,11 @@
+package com.ldt.mylib;
+
+public class MyLib {
+    public static int plus(int a, int b) {
+        return a + b;
+    }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+}
